b49713db8e8d6f1c70b8f948c89004dfa3666319
toby <toby@slackitx.ts.ts>
2023-11-08 20:34:05 +0000

    m3u wip

--- splitsets.sh
+++ splitsets.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 echo "input path to file"
 pwd
-
+ls --literal
 read -r originalmp3file
 echo "splitting: $originalmp3file"
 mp3filename=${originalmp3file::-4}
 ffmpeg -i ./"$originalmp3file" -f segment -segment_time 600 -c copy "$mp3filename"%03d.mp3
+echo "#EXTM3U" > "$mp3filename".m3u
+ls --format=single-column --literal | grep "^.*[000-999]\.mp3$" > "$mp3filename".m3u
 echo "file split"
