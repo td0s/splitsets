74579338d2b2196aa6d79e64210fa509cb1ce83b
toby <toby@slacklap.ts>
2023-11-09 22:11:58 +0000

    Tagging ID3

--- splitsets.sh
+++ splitsets.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 echo "input path to file"
 pwd
-ls --literal
+ls --literal -1 | grep mp3
 read -r originalmp3file
 echo "splitting: $originalmp3file"
 mp3filename=${originalmp3file::-4}
 mkdir split
 ffmpeg -i ./"$originalmp3file" -f segment -segment_time 600 -c copy "split/$mp3filename"%03d.mp3
+# shellcheck disable=SC2164
 cd split
 pwd
 ls
 for track in *.mp3 ; do
-    track_num=${track:-7:-4}
+    track_num=${track:${#track}-7:3}
     echo "Track ${track}"
     echo "Track num $track_num"
-    id3tag --song="${title}" --track=$track_num --album="${originalmp3file}" "${track}"
+    id3tag --song="${track}" --track="$track_num" --album="${originalmp3file}" "${track}"
 done
 
 echo "file split"
